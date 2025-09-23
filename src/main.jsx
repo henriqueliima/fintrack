c0e29f89eca8968b523725ff865d67b06f3bbf5a
Henrique Lima <henriqueliima@users.noreply.github.com>
2025-09-23 13:36:58 -0300

    chore: add react query setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
@@ -28,8 +29,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
